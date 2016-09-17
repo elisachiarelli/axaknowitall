3a24066334b3b75777d25741a434b7532b394755
Anton Dmitrijev <anton@tripledev.ee>
2016-09-17 10:19:55 +0200

    commit java app template

---
+++ src/main/java/ch.hack/App.java
@@ -0,0 +1,28 @@
+package ch.hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableAutoConfiguration
+@ComponentScan("ch.hack")
+public class App {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("*");
+            }
+        };
+    }
+
+    public static void main(String[] args) throws Exception {
+        String a = "blahblah";
+        SpringApplication.run(App.class, args);
+    }
+}
