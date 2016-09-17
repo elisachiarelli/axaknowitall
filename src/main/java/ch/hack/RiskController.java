6923b9e38d82b622a0fb9585dd0e458a392c569c
Anton Dmitrijev <anton@tripledev.ee>
2016-09-17 15:25:57 +0200

    query the database

--- src/main/java/ch/hack/RiskController.java
+++ src/main/java/ch/hack/RiskController.java
@@ -5,7 +5,6 @@ import ch.hack.model.Risk;
 import ch.hack.request.UserAnswer;
 import ch.hack.response.AnswerCheckResult;
 import ch.hack.response.QuestionAnswer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +15,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
